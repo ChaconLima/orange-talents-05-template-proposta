2c8df61c18bb2b76fc06e2c3770dffaf28d24195
Mateus de Assis Chacon Lima <mateusdeassislima@gmail.com>
2021-06-11 15:15:39 -0300

    Implemetanção do fluxo de autenticação e autorização da aplicação, por meio do keycloak e auth2

--- src/main/java/br/com/mateuschacon/proposta/PropostaApplication.java
+++ src/main/java/br/com/mateuschacon/proposta/PropostaApplication.java
@@ -3,9 +3,11 @@ package br.com.mateuschacon.proposta;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @SpringBootApplication
 @EnableFeignClients
+@EnableWebSecurity
 public class PropostaApplication {
 
 	public static void main(String[] args) {
